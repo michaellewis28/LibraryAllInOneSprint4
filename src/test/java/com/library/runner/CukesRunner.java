eaf8254a68e2309925e0a8400e376c762e46738d
Betul Zora <zorabtl@gmail.com>
2023-04-16 20:33:09 -0500

    US01_BZ complete

--- src/test/java/com/library/runner/CukesRunner.java
+++ src/test/java/com/library/runner/CukesRunner.java
@@ -16,7 +16,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features" ,
         glue = "com/library/steps",
         dryRun = false,
-        tags = "@us01"
+        tags = "@B28G5-189"
 )
 public class CukesRunner {
 

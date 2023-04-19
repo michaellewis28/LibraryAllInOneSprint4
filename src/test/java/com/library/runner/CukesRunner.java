f994f51d96bf417084e5d3eb43fc2cf9f45ff1dc
Daniel_B <daniel.balan1995@gmail.com>
2023-04-19 00:21:26 -0400

    US5-B28G5-185 completed

--- src/test/java/com/library/runner/CukesRunner.java
+++ src/test/java/com/library/runner/CukesRunner.java
@@ -13,10 +13,10 @@ import org.junit.runner.RunWith;
                 "me.jvt.cucumber.report.PrettyReports:target/cucumber",
                 "json:target/cucumber.json"
         },
-        features = "src/test/resources/features" ,
+        features = "src/test/resources/features",
         glue = "com/library/steps",
         dryRun = false,
-        tags = "@us01"
+        tags = "@B28G5-191"
 )
 public class CukesRunner {
 
